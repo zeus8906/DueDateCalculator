01caa5687214ff38db290243ba22ecc9bf74e14d
geza <geza_nagy@epam.com>
2017-07-16 20:43:09 +0200

    Initial code

---
+++ src/main/java/org/emarsys/homework/DueDateCalculator.java
@@ -0,0 +1,63 @@
+package org.emarsys.homework;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class DueDateCalculator {
+
+  private static final int NO_OF_WEEKEND_DAYS = 2;
+  private static final int NO_OF_WORKDAYS = 5;
+  private static final int END_WORING_HOURS = 17;
+  private static final int BEGIN_WORKING_HOURS = 9;
+  private static final int WORKING_HOURS = 8;
+
+  public Date calculateDueDate(Date submitDate, int turnAroundTime) {
+    validateSubmissionDate(submitDate);
+    validateTurnAroundTime(turnAroundTime);
+    Calendar resultDate = Calendar.getInstance(Locale.US);
+    resultDate.setTime(submitDate);
+    int hourOfDay = resultDate.get(Calendar.HOUR_OF_DAY);
+    int dayOfWeek = resultDate.get(Calendar.DAY_OF_WEEK) - 1;
+    int daysShouldAdded = calculateDayOffset(turnAroundTime, hourOfDay, dayOfWeek);
+    resultDate.add(Calendar.DAY_OF_MONTH, daysShouldAdded);
+    resultDate.add(Calendar.HOUR_OF_DAY, calculateHourOffset(turnAroundTime, hourOfDay));
+    return resultDate.getTime();
+  }
+
+  private int calculateHourOffset(int turnAroundTime, int hourOfDay) {
+    return hourOfDay + turnAroundTime % WORKING_HOURS >= END_WORING_HOURS
+        ? BEGIN_WORKING_HOURS + turnAroundTime % WORKING_HOURS - END_WORING_HOURS
+        : turnAroundTime % WORKING_HOURS;
+  }
+
+  private int calculateDayOffset(int turnAroundTime, int hourOfDay, int dayOfWeek) {
+    int workDaysShouldAdd = turnAroundTime / WORKING_HOURS
+        + (hourOfDay + turnAroundTime % WORKING_HOURS >= END_WORING_HOURS ? 1 : 0);
+    int weekendDays = NO_OF_WEEKEND_DAYS * (workDaysShouldAdd / NO_OF_WORKDAYS)
+        + (dayOfWeek + workDaysShouldAdd % NO_OF_WORKDAYS > NO_OF_WORKDAYS ? NO_OF_WEEKEND_DAYS : 0);
+    int daysShouldAdded = workDaysShouldAdd + weekendDays;
+    return daysShouldAdded;
+  }
+
+  private void validateTurnAroundTime(int turnAroundTime) {
+    if (turnAroundTime < 1) {
+      throw new IllegalArgumentException("Turn around time must be a positive number!");
+    }
+  }
+
+  private void validateSubmissionDate(Date submitDate) {
+    if (submitDate == null) {
+      throw new IllegalArgumentException("Submission Date cannot be null!");
+    }
+    final Calendar calendar = Calendar.getInstance();
+    calendar.setTime(submitDate);
+    if (calendar.get(Calendar.DAY_OF_WEEK) - 1 > NO_OF_WORKDAYS || calendar.get(Calendar.DAY_OF_WEEK) - 1 < 1
+        || calendar.get(Calendar.HOUR_OF_DAY) < BEGIN_WORKING_HOURS
+        || calendar.get(Calendar.HOUR_OF_DAY) >= END_WORING_HOURS) {
+      throw new IllegalArgumentException("Submission date is not in valid range!");
+    }
+
+  }
+
+}
---
+++ src/test/java/org/emarsys/homework/DueDateCalculatorTest.java
@@ -0,0 +1,193 @@
+package org.emarsys.homework;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
+
+
+public class DueDateCalculatorTest {
+
+  private static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+  private static final DueDateCalculator underTest = new DueDateCalculator();
+
+  @Test
+  public void testCalculateDueDateInDay() throws ParseException {
+    // GIVEN
+    final Date submissionDate = DATE_FORMATTER.parse("2017/07/12 09:12");
+    final int turnAroundTime = 4;
+    final Date expectedResult = DATE_FORMATTER.parse("2017/07/12 13:12");
+    // WHEN
+    final Date dueDate = underTest.calculateDueDate(submissionDate, turnAroundTime);
+    // THEN
+    assertEquals(expectedResult, dueDate);
+  }
+
+
+  @Test
+  public void testCalculateDueDateExactEndOfDay() throws ParseException {
+    // GIVEN
+    final Date submissionDate = DATE_FORMATTER.parse("2017/07/12 09:00");
+    final int turnAroundTime = 8;
+    final Date expectedResult = DATE_FORMATTER.parse("2017/07/13 09:00");
+    // WHEN
+    final Date dueDate = underTest.calculateDueDate(submissionDate, turnAroundTime);
+    // THEN
+    assertEquals(expectedResult, dueDate);
+  }
+
+  @Test
+  public void testCalculateDueDateTomorrow() throws ParseException {
+    // GIVEN
+    final Date submissionDate = DATE_FORMATTER.parse("2017/07/12 12:01");
+    final int turnAroundTime = 5;
+    final Date expectedResult = DATE_FORMATTER.parse("2017/07/13 09:01");
+    // WHEN
+    final Date dueDate = underTest.calculateDueDate(submissionDate, turnAroundTime);
+    // THEN
+    assertEquals(expectedResult, dueDate);
+  }
+
+  @Test
+  public void testCalculateDueDateMoreThenOneDay() throws ParseException {
+    // GIVEN
+    final Date submissionDate = DATE_FORMATTER.parse("2017/07/12 09:01");
+    final int turnAroundTime = 18;
+    final Date expectedResult = DATE_FORMATTER.parse("2017/07/14 11:01");
+    // WHEN
+    final Date dueDate = underTest.calculateDueDate(submissionDate, turnAroundTime);
+    // THEN
+    assertEquals(expectedResult, dueDate);
+  }
+
+  @Test
+  public void testCalculateDueDateFromFirdayToMonday() throws ParseException {
+    // GIVEN
+    final Date submissionDate = DATE_FORMATTER.parse("2017/07/14 13:01");
+    final int turnAroundTime = 5;
+    final Date expectedResult = DATE_FORMATTER.parse("2017/07/17 10:01");
+    // WHEN
+    final Date dueDate = underTest.calculateDueDate(submissionDate, turnAroundTime);
+    // THEN
+    assertEquals(expectedResult, dueDate);
+  }
+
+  @Test
+  public void testCalculateDueDateMoreThanOneWeek() throws ParseException {
+    // GIVEN
+    final Date submissionDate = DATE_FORMATTER.parse("2017/07/12 09:01");
+    final int turnAroundTime = 48;
+    final Date expectedResult = DATE_FORMATTER.parse("2017/07/20 09:01");
+    // WHEN
+    final Date dueDate = underTest.calculateDueDate(submissionDate, turnAroundTime);
+    // THEN
+    assertEquals(expectedResult, dueDate);
+  }
+
+  @Test
+  public void testCalculateDueDateLessThenOneWeekButWeekend() throws ParseException {
+    // GIVEN
+    final Date submissionDate = DATE_FORMATTER.parse("2017/07/12 09:01");
+    final int turnAroundTime = 28;
+    final Date expectedResult = DATE_FORMATTER.parse("2017/07/17 13:01");
+    // WHEN
+    final Date dueDate = underTest.calculateDueDate(submissionDate, turnAroundTime);
+    // THEN
+    assertEquals(expectedResult, dueDate);
+  }
+
+  @Test
+  public void testCalculateDueDateMoreThanOneMonth() throws ParseException {
+    // GIVEN
+    final Date submissionDate = DATE_FORMATTER.parse("2017/07/12 09:01");
+    final int turnAroundTime = 163;
+    final Date expectedResult = DATE_FORMATTER.parse("2017/08/09 12:01");
+    // WHEN
+    final Date dueDate = underTest.calculateDueDate(submissionDate, turnAroundTime);
+    // THEN
+    assertEquals(expectedResult, dueDate);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCalculateDueDateInvalidInputNegativeTurnAroundTime() throws ParseException {
+    // GIVEN
+    final Date submissionDate = DATE_FORMATTER.parse("2017/07/12 09:01");
+    final int turnAroundTime = -1;
+    // WHEN
+    underTest.calculateDueDate(submissionDate, turnAroundTime);
+    // THEN
+    assertTrue("Should not reach this point!", false);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCalculateDueDateInvalidInputZeroTurnAroundTime() throws ParseException {
+    // GIVEN
+    final Date submissionDate = DATE_FORMATTER.parse("2017/07/12 09:01");
+    final int turnAroundTime = 0;
+    // WHEN
+    underTest.calculateDueDate(submissionDate, turnAroundTime);
+    // THEN
+    assertTrue("Should not reach this point!", false);
+  }
+
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCalculateDueDateInvalidInputNullSubmissionDate() throws ParseException {
+    // GIVEN
+    final Date submissionDate = null;
+    final int turnAroundTime = 8;
+    // WHEN
+    underTest.calculateDueDate(submissionDate, turnAroundTime);
+    // THEN
+    assertTrue("Should not reach this point!", false);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCalculateDueDateInvalidInputSubmissionDateUnderRange() throws ParseException {
+    // GIVEN
+    final Date submissionDate = DATE_FORMATTER.parse("2017/07/12 08:59");
+    final int turnAroundTime = 8;
+    // WHEN
+    underTest.calculateDueDate(submissionDate, turnAroundTime);
+    // THEN
+    assertTrue("Should not reach this point!", false);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCalculateDueDateInvalidInputSubmissionDateOverRange() throws ParseException {
+    // GIVEN
+    final Date submissionDate = DATE_FORMATTER.parse("2017/07/12 17:01");
+    final int turnAroundTime = 8;
+    // WHEN
+    underTest.calculateDueDate(submissionDate, turnAroundTime);
+    // THEN
+    assertTrue("Should not reach this point!", false);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCalculateDueDateInvalidInputSubmissionDateOnSunday() throws ParseException {
+    // GIVEN
+    final Date submissionDate = DATE_FORMATTER.parse("2017/07/16 10:01");
+    final int turnAroundTime = 8;
+    // WHEN
+    underTest.calculateDueDate(submissionDate, turnAroundTime);
+    // THEN
+    assertTrue("Should not reach this point!", false);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCalculateDueDateInvalidInputSubmissionDateOnSaturday() throws ParseException {
+    // GIVEN
+    final Date submissionDate = DATE_FORMATTER.parse("2017/07/15 10:01");
+    final int turnAroundTime = 8;
+    // WHEN
+    underTest.calculateDueDate(submissionDate, turnAroundTime);
+    // THEN
+    assertTrue("Should not reach this point!", false);
+  }
+}
